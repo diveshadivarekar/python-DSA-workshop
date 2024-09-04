bafd395d3cba4cd873cd722d941d29ec00288dda
Divesh Adivarekar <diveshadivarekar09@gmail.com>
2024-09-04 14:37:39 +0530

    Create Queue.py

---
+++ DSA/Queue.py
@@ -0,0 +1,40 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == 0
+    
+    def push(self, item):
+        self.items.insert(0, item)
+        
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            return None
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            return None
+        
+    def __str__(self):
+        return str(self.items)
+        
+
+if __name__ == '__main__':
+    q = Queue()
+    
+    q.push(1)
+    q.push(2)
+    q.push(3)
+    q.push(4)
+    
+    print("Queue items are: ", q )
+    
+    print(q.pop())
+    
+    print("Queue items are: ", q )
+    
