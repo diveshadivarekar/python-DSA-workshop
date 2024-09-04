1b90e2ee3e295ea0b7f92dcff308afd44d68faea
Divesh Adivarekar <diveshadivarekar09@gmail.com>
2024-09-04 15:42:52 +0530

    Create bubble.py

---
+++ DSA/bubble.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    
+    return arr
+
+arr = [5,3,6,7,8,1]
+
+print("unsorted array : ",arr)
+print("sorted array : ",bubble_sort(arr))
